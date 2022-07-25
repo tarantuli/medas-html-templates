05a40fb8b23a6917f26c8c7b2506a80c0b6ee0bb
Patrick Morpey <patrick@morphp.nl>
2022-07-25 10:59:52 +0200

    Replaced placeholders

--- src/HtmlTemplatesPackage.php
+++ src/HtmlTemplatesPackage.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-namespace Medas\Placeholder;
+namespace Medas\HtmlTemplates;
 
 use Medas\ServiceManager\{AsSingleton, BasePackage};
 
-class PlaceholderPackage extends BasePackage
+class HtmlTemplatesPackage extends BasePackage
 {
     use AsSingleton;
 
